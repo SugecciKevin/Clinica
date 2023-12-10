99306c0136c27793ce1e1a24f07174fd266d6e3a
axsgro <ale.silva.gro@outlook.es>
2023-12-10 13:50:47 -0600

    portada

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,22 +1,20 @@
 @extends('base')
 
 @section('contenido')
-    <div class="container mt-5 d-flex justify-content-center"> <!-- Agregado justify-content-center -->
+    <div class="container mt-5 d-flex justify-content-center">
         <div class="row">
             <div class="col-md-6 text-center">
-                <h2>Nombre 1</h2>
-                <p>Instituto: ITA</p>
+                <h2>Instituto: Instituto Tecnológico de Aguascalientes</h2>
                 <p>Materia: Programacion Web</p>
                 <p>Unidad 5</p>
                 <p>Proyecto web</p>
-                <p>Profesor: Hazael</p>
-                <img src="/ruta/a/la/primera/imagen.jpg" alt="Imagen 1" class="img-fluid mt-3">
+                <p>Profesor: Hazael Vazquez</p>
             </div>
         </div>
 
         <!-- Tarjeta 1 -->
         <div class="card mt-5">
-            <img src="/ruta/a/la/imagen/tarjeta1.jpg" class="card-img-top" alt="Imagen Tarjeta 1">
+            <img src="{{ asset('img/Alexis.jpeg') }}" alt="Nombre Alternativo" class="img-fluid">
             <div class="card-body text-center">
                 <h5 class="card-title">Jose Alexis Silva Guerrero</h5>
             </div>
@@ -24,7 +22,7 @@
 
         <!-- Tarjeta 2 -->
         <div class="card mt-5">
-            <img src="/ruta/a/la/imagen/tarjeta2.jpg" class="card-img-top" alt="Imagen Tarjeta 2">
+            <img src="{{ asset('img/Kevin.jpeg') }}" alt="Nombre Alternativo" class="img-fluid">
             <div class="card-body text-center">
                 <h5 class="card-title">Kevin Ivan Ramirez Chairez</h5>
             </div>
